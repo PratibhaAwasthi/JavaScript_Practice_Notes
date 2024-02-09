0ac3f138af4f9616cf56b811f5b35056c0ac48b5
PratibhaAwasthi <pratibhaawasthi17@gmail.com>
2024-02-09 14:54:28 -0800

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+let emailAddress = "xyz@gmail.com"; //mutable
+const firstName = "ABCDE"; //not mutable
+age = 32; //not the best way to declare variable
+var school = "st mary`s school"; //old ways to declare
+
+console.log(emailAddress);
+
+console.table([firstName, age, school]);
