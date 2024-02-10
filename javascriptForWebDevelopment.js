1e84e37e98fff9475150ef8188e33ad45e3e3d53
PratibhaAwasthi <pratibhaawasthi17@gmail.com>
2024-02-10 01:33:28 -0800

    fundamental theory

---
+++ javascriptForWebDevelopment.js
@@ -0,0 +1,31 @@
+//Role of JS in Web Development
+
+/*
+We design web pages using threee techs:
+
+1.HTML: Noun of the webpage. 
+2.CSS: Adjectives of the webpage
+3.JS: Verb of the webpage. 
+
+Example: 
+
+HTML:
+<p> " I'm a paragraph " </p>
+
+CSS:
+p{color:red}
+
+JS:
+p.hide()
+ 
+*/
+
+//Enviroment Setup
+
+/*
+
+JS was taught and used on browser. Because the transpiler was atatched with the browser. It was standalone available but was difficult to setup and configure. Only high end SDEs use to use those transpiler. Most of the rest, would take a use of transpiler available in the browser.
+
+Later, the transpiler available in browser was  made standalone availavble. Which we now call Nodejs. Nodejs helps us to provide enviroment to run JavaScript. Additionally, there are more transpiler available. 
+
+*/
